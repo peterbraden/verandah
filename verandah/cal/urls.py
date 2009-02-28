99745fbd7130c66cd142c954681310ded0755a40
Peter Braden <PeterBraden@PeterBraden.co.uk>
2009-02-28 12:41:24 +0000

    corrected urls

--- verandah/cal/urls.py
+++ verandah/cal/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 import settings
 
 urlpatterns = patterns('',
-    url(r'^$', 'cal.views.dashboard', name = 'calendar'),
-    url(r'^month/(?P<year>\d{4})/(?P<month>\d{2})$', 'cal.views.dashboard', name = 'calendar_month'),
+    url(r'$', 'cal.views.dashboard', name = 'calendar'),
+    url(r'month/(?P<year>\d{4})/(?P<month>\d{2})$', 'cal.views.dashboard', name = 'calendar_month'),
 )
